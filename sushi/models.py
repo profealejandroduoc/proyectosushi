3dd9daf6104cf190118e93ee8b0f13458ab3ee81
El profesor <al.sepulvedam@profesor.duoc.cl>
2024-05-24 10:28:17 -0400

    pruieba de modelo

--- sushi/models.py
+++ sushi/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    producto=models.CharField(max_length=50, null=False)
+    imagen=models.ImageField(null=True)
+    descripcion=models.CharField(max_length=50, null=False)
+    valor=models.IntegerField(null=False)
+
+
+    def __str__(self):
+        return self.producto
